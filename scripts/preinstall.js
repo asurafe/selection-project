b0851903e66a3802322be5f660d3b2e34ab77647
ausra <ausra@ausradeMacBook-Pro.local>
2023-06-05 16:52:24 +0800

    feat: 强制使用pnpm包管理器

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,7 +1,7 @@
-if (!/pnpm/.test(process.env.npm_execpath || '')) {
-    console.warn(
-      `\u001b[33m这个项目必须使用pnpm来进行包管理 ` +
-      `请使用pnpm重新进行下载.\u001b[39m\n`,
-    )
-    process.exit(1)
-  }
+if (!/pnpm/.test(process.env.npm_execpath || "")) {
+  console.warn(
+    `\u001b[33m这个项目必须使用pnpm来进行包管理 ` +
+      `请使用pnpm重新进行下载.\u001b[39m\n`
+  );
+  process.exit(1);
+}
