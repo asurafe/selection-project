0603168243568d50a140965ae19026ce8d00c7ee
ausra <ausra@ausradeMacBook-Pro.local>
2023-06-05 18:05:40 +0800

    feat: 配置element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>硅谷甄选</h1>
+  <el-config-provider :locale="zhCn">
+    <el-button type="primary" @click="handle">你好帅</el-button>
+    <el-pagination
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000"
+    />
+  </el-config-provider>
+
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+const handle = () => {
+  ElMessage.warning('你好👋')
+}
+</script>
 <style scoped></style>
