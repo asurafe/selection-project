7e39f97314063167135be53d0b41e4a616bc9f0d
ausra <ausra@ausradeMacBook-Pro.local>
2023-06-06 11:11:12 +0800

    feat: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import vue from "@vitejs/plugin-vue";
-import path from 'path'
+import path from "path";
 import { defineConfig } from "vite";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
@@ -36,7 +36,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-        "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
-    }
-  }
+      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
+    },
+  },
 });
