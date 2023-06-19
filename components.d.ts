f0429521bf04d47a7dee12f8f8458cada00c21b8
ausra <ausra@ausradeMacBook-Pro.local>
2023-06-19 18:31:05 +0800

    feat: test

--- components.d.ts
+++ components.d.ts
@@ -5,38 +5,38 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {};
+export {}
 
-declare module "@vue/runtime-core" {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ElAside: typeof import("element-plus/es")["ElAside"];
-    ElBreadcrumb: typeof import("element-plus/es")["ElBreadcrumb"];
-    ElBreadcrumbItem: typeof import("element-plus/es")["ElBreadcrumbItem"];
-    ElButton: typeof import("element-plus/es")["ElButton"];
-    ElCard: typeof import("element-plus/es")["ElCard"];
-    ElColumn: typeof import("element-plus/es")["ElColumn"];
-    ElConfigProvider: typeof import("element-plus/es")["ElConfigProvider"];
-    ElContainer: typeof import("element-plus/es")["ElContainer"];
-    ElDialog: typeof import("element-plus/es")["ElDialog"];
-    ElDropdown: typeof import("element-plus/es")["ElDropdown"];
-    ElDropdownItem: typeof import("element-plus/es")["ElDropdownItem"];
-    ElDropdownMenu: typeof import("element-plus/es")["ElDropdownMenu"];
-    ElForm: typeof import("element-plus/es")["ElForm"];
-    ElFormItem: typeof import("element-plus/es")["ElFormItem"];
-    ElHeader: typeof import("element-plus/es")["ElHeader"];
-    ElIcon: typeof import("element-plus/es")["ElIcon"];
-    ElInput: typeof import("element-plus/es")["ElInput"];
-    ElMain: typeof import("element-plus/es")["ElMain"];
-    ElMenu: typeof import("element-plus/es")["ElMenu"];
-    ElMenuItem: typeof import("element-plus/es")["ElMenuItem"];
-    ElPagination: typeof import("element-plus/es")["ElPagination"];
-    ElScrollbar: typeof import("element-plus/es")["ElScrollbar"];
-    ElSubItemMenu: typeof import("element-plus/es")["ElSubItemMenu"];
-    ElSubMenu: typeof import("element-plus/es")["ElSubMenu"];
-    ElTable: typeof import("element-plus/es")["ElTable"];
-    ElTableColumn: typeof import("element-plus/es")["ElTableColumn"];
-    RouterLink: typeof import("vue-router")["RouterLink"];
-    RouterView: typeof import("vue-router")["RouterView"];
-    SvgIcon: typeof import("./src/components/SvgIcon/index.vue")["default"];
+    ElAside: typeof import('element-plus/es')['ElAside']
+    ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
+    ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
+    ElButton: typeof import('element-plus/es')['ElButton']
+    ElCard: typeof import('element-plus/es')['ElCard']
+    ElColumn: typeof import("element-plus/es")["ElColumn"]
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDialog: typeof import("element-plus/es")["ElDialog"]
+    ElDropdown: typeof import('element-plus/es')['ElDropdown']
+    ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
+    ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElForm: typeof import("element-plus/es")["ElForm"]
+    ElFormItem: typeof import("element-plus/es")["ElFormItem"]
+    ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import("element-plus/es")["ElInput"]
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElPagination: typeof import('element-plus/es')['ElPagination']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
+    ElSubItemMenu: typeof import("element-plus/es")["ElSubItemMenu"]
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    ElTable: typeof import('element-plus/es')['ElTable']
+    ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    SvgIcon: typeof import('./src/components/SvgIcon/index.vue')['default']
   }
 }
