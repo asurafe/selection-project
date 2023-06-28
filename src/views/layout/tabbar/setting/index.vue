157ade5904d8e5d281f0f503b318bf338910a6a0
ausra <ausra@ausradeMacBook-Pro.local>
2023-06-28 15:57:15 +0800

    feat: 完成暗黑模式和主题

--- src/views/layout/tabbar/setting/index.vue
+++ src/views/layout/tabbar/setting/index.vue
@@ -1,7 +1,32 @@
 <template>
   <el-button circle icon="Refresh" @click="handleRefresh"></el-button>
   <el-button circle icon="FullScreen" @click="handleFullScreen"></el-button>
-  <el-button circle icon="Setting"></el-button>
+  <el-popover placement="bottom" title="主题设置" :width="200" trigger="hover">
+    <el-form>
+      <el-form-item label="主题颜色">
+        <el-color-picker
+          v-model="color"
+          show-alpha
+          :predefine="predefineColors"
+          size="small"
+          @change="setColor"
+        />
+      </el-form-item>
+      <el-form-item label="暗黑模式">
+        <el-switch
+          v-model="dark"
+          size="small"
+          inline-prompt
+          active-icon="MoonNight"
+          inactive-icon="Sunny"
+          @change="changeDark"
+        />
+      </el-form-item>
+    </el-form>
+    <template #reference>
+      <el-button circle icon="Setting"></el-button>
+    </template>
+  </el-popover>
   <img :src="useUserStore.avatar" alt="" />
   <el-dropdown>
     <span class="el-dropdown-link" style="cursor: pointer">
@@ -18,12 +43,32 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { userStore } from "@/store/modules/user";
 import useLayOutSettingStore from "@/store/setting";
 import { useRouter } from "vue-router";
 
+const dark = ref<boolean>(false)
 const $router = useRouter();
 const useUserStore = userStore();
+const color = ref('rgba(255, 69, 0, 0.68)')
+const predefineColors = ref([
+  '#ff4500',
+  '#ff8c00',
+  '#ffd700',
+  '#90ee90',
+  '#00ced1',
+  '#1e90ff',
+  '#c71585',
+  'rgba(255, 69, 0, 0.68)',
+  'rgb(255, 120, 0)',
+  'hsv(51, 100, 98)',
+  'hsva(120, 40, 94, 0.5)',
+  'hsl(181, 100%, 37%)',
+  'hsla(209, 100%, 56%, 0.73)',
+  '#c7158577',
+])
+
 
 const layOutSettingStore = useLayOutSettingStore();
 // 刷新
@@ -39,6 +84,15 @@ const handleFullScreen = () => {
     document.exitFullscreen();
   }
 };
+const changeDark = () => {
+  let html = document.documentElement
+  dark.value ? (html.className = 'dark') : (html.className = '')
+}
+
+const setColor = () => {
+  let html = document.documentElement
+  html.style.setProperty('--el-color-primary', color.value)
+}
 // 退出登陆
 const handleLayout = async () => {
   try {
